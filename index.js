5280c270a2d9bd914f239f63e5eb1bba110eb0a4
crumbz <133969306+left-no-crumbz@users.noreply.github.com>
2024-05-23 13:01:44 +0800

    feat: Add getComputerChoice function

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 console.log("Hello World!");
 
-let generateChoice = (min, max) => Math.random() * (max - min) + min;
+let generateChoice = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+function getComputerChoice(choice) {
+    switch(choice) {
+        case 1: return "Rock";
+        case 2: return "Paper";
+        case 3: return "Scissors";
+    }
+}
 
