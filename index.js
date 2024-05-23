35d27b533650b81132abd01bdf5f8d2c42971796
crumbz <133969306+left-no-crumbz@users.noreply.github.com>
2024-05-23 15:10:16 +0800

    feat: Add playRound function

--- index.js
+++ index.js
@@ -10,3 +10,34 @@ function getComputerChoice(choice) {
     }
 }
 
+let getHumanChoice = () => prompt("Please enter a choice: ");
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    let upperHumanChoice = humanChoice.toUpperCase();
+    let upperComputerChoice = computerChoice.toUpperCase();
+    
+    let humanWin = (upperHumanChoice === "ROCK" && upperComputerChoice === "SCISSORS" ||
+    upperHumanChoice === "SCISSORS" && upperComputerChoice === "PAPER" ||
+    upperHumanChoice === "PAPER" && upperComputerChoice === "ROCK");
+    
+    console.log(`Human chose: ${upperHumanChoice}`);
+    console.log(`Computer chose: ${upperComputerChoice}`);
+
+    let result = upperHumanChoice === upperComputerChoice ?
+        console.log(`Draw! ${upperHumanChoice} is equal to ${upperComputerChoice}`) :
+        win = humanWin ? humanScore++ : computerScore++;
+        
+    
+    if (humanScore > computerScore) {
+        console.log(`You win! ${upperHumanChoice} beats ${upperComputerChoice}`);
+    } else if (humanScore < computerScore){
+        console.log(`You lose! ${upperComputerChoice} beats ${upperHumanChoice}`);
+    }
+}
+
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice(generateChoice(1, 3));
+playRound(humanChoice, computerChoice);
